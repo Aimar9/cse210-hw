e67c157fd91d0f0031e2acdc093ffec9f5479719
Aimar9 <aimar.25alcos@gmail.com>
2024-10-09 02:00:43 -0500

    prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq.Expressions;
 
 class Program
 {
@@ -6,5 +7,30 @@ class Program
     {
         // This is prep 2
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("What is your note? ");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+
+        if (number >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (number >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (number >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (number >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else if (number < 60)
+        {
+            Console.WriteLine("F");
+        }
     }
 }
